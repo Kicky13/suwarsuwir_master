d3085c7ee30be6b865d99150a0054b7b5fede510
Kicky13 <32523482+Kicky13@users.noreply.github.com>
2018-02-10 11:08:01 +0700

    11:07

--- app/Http/Controllers/PeramalanController.php
+++ app/Http/Controllers/PeramalanController.php
@@ -33,18 +33,38 @@ class PeramalanController extends Controller
 
     public function ramal(Request $request)
     {
-        $date = Carbon::now(+7);
         $produk = $request->produk_id;
-        $single = $this->single($produk);
-//        $double = $this->double($single, $produk);
-//        $triple = $this->triple($double, $produk);
-        echo $single;
+        $date = Carbon::now(+7);
+        $recent = Peramalan::where('produk_id', $produk)->whereBetween('tanggal_peramalan', [$date->startOfMonth(), $date->endOfMonth()]);
+        if ($recent->count() == 0){
+            $single = $this->single($produk);
+            $double = $this->double($single, $produk);
+            $triple = $this->triple($double, $produk);
+            $total = collect([['at' => $triple['at1'], 'pe' => $triple['pe1']], ['at' => $triple['at2'], 'pe' => $triple['pe2']], ['at' => $triple['at3'], 'pe' => $triple['pe3']]]);
+            $at = $total->max('at');
+            $pe = $total->min('pe');
+            Peramalan::create([
+                'produk_id' => $produk,
+                'nilai_aktual' => $triple['xt'],
+                'nilai_single' => $triple['at1'],
+                'nilai_double' => $triple['at2'],
+                'nilai_triple' => $triple['at3'],
+                'mape_single' => $triple['pe1'],
+                'mape_double' => $triple['pe2'],
+                'mape_triple' => $triple['pe3'],
+                'nilai_hasil' => $at,
+                'mape_hasil' => $pe
+            ]);
+            echo 'sukses';
+        } else {
+            echo 'exist';
+        }
     }
 
     public function single($produk)
     {
         $date = Carbon::now(+7);
-        $last = $date->subMonth(1);
+        $last = Carbon::now(+7)->subMonth(1);
         $recent = Peramalan::where('produk_id', $produk)->whereBetween('tanggal_peramalan', [$last->startOfMonth(), $last->endOfMonth()]);
         $penjualan = DB::table('permintaan_produk as pp')->join('permintaan as p', 'p.id', '=', 'pp.permintaan_id')->where('validasi_id', '=', 3)->where('produk_id', '=', $produk)->whereYear('tanggal_terjual', '=', $date->year)->whereMonth('tanggal_terjual', '=', $date->month);
         $xt = $penjualan->sum('jumlah_permintaan');
@@ -64,8 +84,7 @@ class PeramalanController extends Controller
 
     public function double($data, $produk)
     {
-        $date = Carbon::now(+7);
-        $last = $date->subMonth(1);
+        $last = Carbon::now(+7)->subMonth(1);
         $recent = Peramalan::where('produk_id', $produk)->whereBetween('tanggal_peramalan', [$last->startOfMonth(), $last->endOfMonth()]);
         if ($recent->count() == 0){
             $at2 = $data['xt'];
@@ -74,22 +93,37 @@ class PeramalanController extends Controller
         }
         $at = (2*$data['at1'])-$at2;
         $bt = (0.4/(1-0.4))*($data['at1']-$at2);
-        $ftm1 = $at+$bt;
-        if ($data['xt']-$ftm1 == 0){
+        $ftm = $at+$bt;
+        if ($data['xt']-$ftm == 0){
             $pe = 0;
         } else {
-            $pe = (abs($data['xt']-$ftm1)/$data['xt'])*100;
+            $pe = (abs($data['xt']-$ftm)/$data['xt'])*100;
         }
-        $result = collect($data, ['at2' => $at2, 'at' => $at, 'bt' => $bt, 'pe2' => $pe]);
-        $data->put('at2', $at2);
-        $data->put('at', $at);
-        $data->put('bt', $bt);
+        $data->put('at2', $ftm);
         $data->put('pe2', $pe);
         return $data;
     }
 
-    public function triple()
+    public function triple($data, $produk)
     {
-
+        $last = Carbon::now(+7)->subMonth(1);
+        $recent = Peramalan::where('produk_id', $produk)->whereBetween('tanggal_peramalan', [$last->startOfMonth(), $last->endOfMonth()]);
+        if ($recent->count() == 0){
+            $at3 = $data['xt'];
+        } else {
+            $at3 = 0;
+        }
+        $at = (3*$data['at1'])-(3*$data['at2'])+$at3;
+        $bt = 0.4/(2*(1-0.4))*((6-(5*0.4)*$data['at1'])-(10-(8*0.4)*$data['at2'])+(4-(3*0.4)*$at3));
+        $ct = pow(0.4, 2)/pow(1-0.4, 2)*($data['at1']-(2*$data['at2'])+$at3);
+        $ftm = $at+$bt+(0.5*$ct);
+        if ($data['xt']-$ftm == 0){
+            $pe = 0;
+        } else {
+            $pe = (abs($data['xt']-$ftm)/$data['xt'])*100;
+        }
+        $data->put('at3', $ftm);
+        $data->put('pe3', $pe);
+        return $data;
     }
 }
--- app/Peramalan.php
+++ app/Peramalan.php
@@ -7,7 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Peramalan extends Model
 {
     protected $table = 'peramalan';
-    public $timestamps = 'tanggal_peramalan';
+    public $timestamps = false;
+    protected $fillable = ['produk_id', 'nilai_aktual', 'nilai_single', 'nilai_double', 'nilai_triple', 'mape_single', 'mape_double', 'mape_triple', 'nilai_hasil', 'mape_hasil'];
 
     public function produk()
     {
